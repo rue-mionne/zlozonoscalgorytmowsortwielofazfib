6f1f7569963f9dfaccb6f7a4356678f257ae6a20
Laura K <laura.konieczna.1999@gmail.com>
2023-01-19 23:25:29 +0100

    Czesciowe zakomentowanie dystrybucji

--- dystrybucja.cpp
+++ dystrybucja.cpp
@@ -6,56 +6,62 @@
 #include <istream>
 #include <vector>
 #include <limits>
-#include <iostream> //debugging
+
+//
+//konnstruktor obiektu: inicjalizacja liczby elementow i podpiecie obiektu z dzielonymi zmiennymi
+//
 
 Dystrybucja::Dystrybucja(int size, ZmiennePomSortowania* pojemnik):n(size){
 	this->pojemnik=pojemnik;
-	std::cout << "\nDystrkonstr";
 }
 
 void Dystrybucja::dzialaj(){
 	//otwarcie plikow
 	for(int i=0; i<4; i++){
-		pojemnik->opiekunowie[i]=new FileHandler(pojemnik->pliki[i]);
-		pojemnik->opiekunowie[i]->otworzPlik();
+		pojemnik->opiekunowie[i]=new FileHandler(pojemnik->pliki[i]);//stworzenie opiekunow plikow
+		pojemnik->opiekunowie[i]->otworzPlik();//opiekunowie otwieraja swoje pliki
 	}
 	int suma=0;
 	//faza wyliczenia przydziału
-	int * tablicaPodzialu = wyliczPodzial();
-	while(suma<n){
-		przydzielPartie(tablicaPodzialu);	
-		delete tablicaPodzialu;	
-		tablicaPodzialu = wyliczPodzial(); 
-		suma = pojemnik->FibTab[3].back(); 
-		std::cout <<suma << "\n";
+	int * tablicaPodzialu = wyliczPodzial(); //wyliczenie pierwszego przydzialu
+	while(suma<n){//dopoki nie zostalu rodzielone wszystkie elementy
+		przydzielPartie(tablicaPodzialu);	//przydziel elementy wedlug ilosci wyliczonej w tablicy
+		delete tablicaPodzialu;	 //zwolnij miejsce po niepotrzebnej tablicy
+		tablicaPodzialu = wyliczPodzial(); //wylicz kolejny podzial
+		suma = pojemnik->FibTab[3].back(); //zaktualizuj liczbe przydzielonych elementow
 	}
-	pojemnik->opiekunowie[3]->strumienOut.seekp(-1, std::ios_base::end);	
-	pojemnik->opiekunowie[3]->strumienOut << " ";
-	for(int i=suma-n; i>0; i--){
-		pojemnik->opiekunowie[3]->strumienOut << std::numeric_limits<int>::max() << " ";
+	pojemnik->opiekunowie[3]->strumienOut.seekp(-1, std::ios_base::end); //ustaw wskaznik zapisu na przedostatnie miejsce pliku 4
+	for(int i=suma-n; i>0; i--){//az do momentu kiedy ilosc elementow nie bedzie sie zgadzala z ciagiem fibonacchiego
+		pojemnik->opiekunowie[3]->strumienOut << std::numeric_limits<int>::max() << " ";//dodaj na koniec maksymalna wartosc int, odpowiednik plus nieskonczonosci
 	}
-	pojemnik->opiekunowie[3]->odswiez();
-	przydzielPartie(tablicaPodzialu);
-	delete tablicaPodzialu;
+	pojemnik->opiekunowie[3]->odswiez();//upewnij sie, ze warotsci zostaly zapisane w pliku, a nie utknely w buforze
+	przydzielPartie(tablicaPodzialu);//dokonaj ostatniego podzialu
+	delete tablicaPodzialu;//zwolnij miejsce po tablicy podzialu
 
 	for(int i=0; i<4; i++){
-		pojemnik->opiekunowie[i]->odswiez();
+		pojemnik->opiekunowie[i]->odswiez();//upewnij sie ze kazdy plik zostal zapisany
 	}
 
 }
 
+//
+//funkcja wyliczajaca kolejne rzedy tablicy slozacej do wyznaczania przydzialu
+//
 void Dystrybucja::wyliczRzad(){
 
-	std::cout << "\nRząd";
 
-	if(pojemnik->FibTab[0].empty()){
-		pojemnik->FibTab[0].emplace_back(1);
+	if(pojemnik->FibTab[0].empty()){//jesli to jest pierwszy rzad
+		pojemnik->FibTab[0].emplace_back(1); //zainicjuj tablice wartosciami 1, 1, 2
 		pojemnik->FibTab[1].emplace_back(1);
 		pojemnik->FibTab[2].emplace_back(2);
-		wyliczSume();
+		wyliczSume(); //uzupenij ostatni vector o sume
 	}
+	//
+	//rzedy wyliczane wedlug	|f1|f2|f3|
+	//				|f2|f3|f2+f3|
+	//
 	else{
-		pojemnik->FibTab[0].emplace_back(pojemnik->FibTab[1].back());
+		pojemnik->FibTab[0].emplace_back(pojemnik->FibTab[1].back()); 
 		pojemnik->FibTab[1].emplace_back(pojemnik->FibTab[2].back());
 		pojemnik->FibTab[2].emplace_back(pojemnik->FibTab[0].back()+pojemnik->FibTab[1].back());
 		wyliczSume();
@@ -63,14 +69,12 @@ void Dystrybucja::wyliczRzad(){
 }
 
 void Dystrybucja::wyliczSume(){
-	std::cout << "\nSuma";
 
 
 	pojemnik->FibTab[3].emplace_back(pojemnik->FibTab[0].back()+pojemnik->FibTab[1].back()+pojemnik->FibTab[2].back());
 }
 
 int* Dystrybucja::wyliczPodzial(){
-	std::cout << "\nPodzial";
 	if(pojemnik->FibTab[0].size()==0){
 		wyliczRzad();
 		int* tab = new int [4];
@@ -82,28 +86,23 @@ int* Dystrybucja::wyliczPodzial(){
 	int tablicaRoznicy[2][4];
 	for(int i = 0; i<4; i++){
 		tablicaRoznicy[0][i]=pojemnik->FibTab[i].back();
-		std::cout << tablicaRoznicy[0][i]<<std::endl;
 	}
 	wyliczRzad();
 	for(int i = 0; i<4; i++){
 		tablicaRoznicy[1][i]=pojemnik->FibTab[i].back();
-		std::cout << tablicaRoznicy[1][i]<<std::endl;
 	}
 	int * tablicaPodzialu = new int [4];
 	for(int i=0; i<4; i++){
 		tablicaPodzialu[i]=tablicaRoznicy[1][i] - tablicaRoznicy[0][i];
-		std::cout << tablicaPodzialu[i] << std::endl;
 	}
 	return tablicaPodzialu;
 }
 
 void Dystrybucja::przydzielPartie(int* tablicaPodzialu){
-	std::cout << "rozdziel\n";
 	for(int i = 0; i<3; i++){
 		for(int j = 0; j<tablicaPodzialu[i]; j++){
 			int temp;
 			pojemnik->opiekunowie[3]->strumienIn >> temp;
-			std::cout << temp << std::endl;
 			pojemnik->opiekunowie[i]->strumienOut << temp << " ";
 		}
 	}
