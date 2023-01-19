2b15e4280e96989d87c5b0feb372c883fc35d2e4
Laura K <laura.konieczna.1999@gmail.com>
2023-01-19 23:53:10 +0100

    Dodano komentarze do dystrybucja.cpp

--- dystrybucja.cpp
+++ dystrybucja.cpp
@@ -67,43 +67,51 @@ void Dystrybucja::wyliczRzad(){
 		wyliczSume();
 	}
 }
-
+//
+//funkcja wyliczajaca sume wydystrybuowanych liczb
+//
 void Dystrybucja::wyliczSume(){
 
 
-	pojemnik->FibTab[3].emplace_back(pojemnik->FibTab[0].back()+pojemnik->FibTab[1].back()+pojemnik->FibTab[2].back());
+	pojemnik->FibTab[3].emplace_back(pojemnik->FibTab[0].back()+pojemnik->FibTab[1].back()+pojemnik->FibTab[2].back());//suma=elementy w pliku 1, 2 i 3
 }
 
+//
+//funkcja wyliczajaca tablice roznicy miedzy 2 ostatnimi rzedzami FibTab = ilosc elementow do przydzielenia w kolejnej iteracji
+//
 int* Dystrybucja::wyliczPodzial(){
-	if(pojemnik->FibTab[0].size()==0){
+	if(pojemnik->FibTab[0].size()==0){//jesli jest tylko jeden rzad
 		wyliczRzad();
 		int* tab = new int [4];
 		for(int i = 0; i<4; i++){
-			tab[i]=pojemnik->FibTab[i].back();
+			tab[i]=pojemnik->FibTab[i].back();//przepisz wartosci do tablicy przydzialu
 		}
 		return tab;
 	}
-	int tablicaRoznicy[2][4];
+	int tablicaRoznicy[2][4]; //tymczasowa tablica
 	for(int i = 0; i<4; i++){
-		tablicaRoznicy[0][i]=pojemnik->FibTab[i].back();
+		tablicaRoznicy[0][i]=pojemnik->FibTab[i].back(); //zapisz do tymczasowej tablicy ostatni rzad
 	}
-	wyliczRzad();
+	wyliczRzad(); //wylicz kolejny
 	for(int i = 0; i<4; i++){
-		tablicaRoznicy[1][i]=pojemnik->FibTab[i].back();
+		tablicaRoznicy[1][i]=pojemnik->FibTab[i].back();//zapisz do kolejnego rzedu tablicy nowy ostatni rzad FibTab
 	}
 	int * tablicaPodzialu = new int [4];
 	for(int i=0; i<4; i++){
-		tablicaPodzialu[i]=tablicaRoznicy[1][i] - tablicaRoznicy[0][i];
+		tablicaPodzialu[i]=tablicaRoznicy[1][i] - tablicaRoznicy[0][i];//wylicz roznice miedzy nimi
 	}
 	return tablicaPodzialu;
 }
 
+//
+//funkcja rozdzielajaca elementy do plikow
+//
 void Dystrybucja::przydzielPartie(int* tablicaPodzialu){
-	for(int i = 0; i<3; i++){
-		for(int j = 0; j<tablicaPodzialu[i]; j++){
+	for(int i = 0; i<3; i++){ //dla kazdego pliku 1-3
+		for(int j = 0; j<tablicaPodzialu[i]; j++){ //zapisz tyle elementow ile wyliczono w tablicy przydzialu
 			int temp;
-			pojemnik->opiekunowie[3]->strumienIn >> temp;
-			pojemnik->opiekunowie[i]->strumienOut << temp << " ";
+			pojemnik->opiekunowie[3]->strumienIn >> temp;//pobierz element z pliku 4
+			pojemnik->opiekunowie[i]->strumienOut << temp << " "; //zapisz w zapelnianym pliku
 		}
 	}
 }
